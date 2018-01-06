3f82dedb338554818f46baccfb501a3ea8b2c048
Denis <dstiven@i.ua>
2018-01-06 15:47:38 +0200

    Created Env annotation

---
+++ src/main/java/ua/dp/skillsup/reflection/annotations/Env.java
@@ -0,0 +1,14 @@
+package ua.dp.skillsup.reflection.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by Denis Berezanskiy on 15.12.17.
+ */
+@Target(ElementType.PARAMETER)
+public @interface Env {
+    String value();
+}
