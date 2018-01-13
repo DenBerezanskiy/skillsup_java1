304264781350f4895457731558d32cfd0e66c8b8
Denis <dstiven@i.ua>
2018-01-13 20:34:10 +0200

    Transfer on working days test implemented. Fails with 'failNotEquals' error

--- src/test/java/ua/dp/skillsup/tdd/FeeServiceTest.java
+++ src/test/java/ua/dp/skillsup/tdd/FeeServiceTest.java
@@ -1,7 +1,11 @@
 package ua.dp.skillsup.tdd;
 
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 import org.mockito.Mockito;
+
+import java.util.Date;
 
 import static org.junit.Assert.*;
 
@@ -26,5 +30,14 @@ WeekendService weekendService;
     weekendService = Mockito.mock(WeekendService.class);
     feeService = new FeeService(holidayService,weekendService);
     accountService = new AccountService(feeService);
+}
+@Test
+public void transferOnWorkingDays()
+{
+    Mockito.when(weekendService.isWeekend()).thenReturn(false);
+    Mockito.when(holidayService.isHoliday(new Date())).thenReturn(false);
+    accountService.transferMoney(sender,recipient,100);
+    Assert.assertEquals(0,sender.getAmount(),0.1);
+    Assert.assertEquals(99,recipient.getAmount(),0.1);
 }
 }
